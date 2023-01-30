3ebc73ed675bff4352432c09f76efd869525837a
Maclemon17 <maclemon17@gmail.com>
2023-01-30 20:37:20 +0100

    notes comp

---
+++ src/components/notes/NoteContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './NoteContainer.module.css'
+
+const NoteContainer = () => {
+    return (
+        <main>
+            <div className={`container ${styles.notes}`}>
+                <div className={styles.actionsBox}>
+                    <h1>Welcome, Maclemon</h1>
+
+                    <div className={styles.buttons}>
+                        <button>Profile</button>
+                        <button>Log out</button>
+                    </div>
+                </div>
+
+                <div className={styles.notesGroup}>
+                    <div className="note">
+                        <h3 className={styles.title}>Title</h3>
+                        <p className="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Delectus vel sequi neque voluptatum repellendus vitae unde ratione odio quae facilis.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default NoteContainer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
+import NoteContainer from '../components/notes/NoteContainer'
 
 const Home = () => {
     return (
         <>
           <Header />
+          <NoteContainer />
           {/* <Footer /> */}
         </>
     )
